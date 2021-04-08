b9436b1f7fccf9bedf32509e1b860b52d543597a
German <k4tger@gmail.com>
2021-04-08 16:08:49 +0300

    Добавлено наименование теста и шагов

--- src/test/java/tests/AndroidTestOFSelenide.java
+++ src/test/java/tests/AndroidTestOFSelenide.java
@@ -2,6 +2,7 @@ package tests;
 
 import io.appium.java_client.MobileBy;
 import lifecycle.MobileTestLifeCycleExtension;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -13,13 +14,15 @@ import static io.qameta.allure.Allure.step;
 
 @ExtendWith(MobileTestLifeCycleExtension.class)
 public class AndroidTestOFSelenide {
+
 	@Test
+	@DisplayName("Проверка поиска по наименованию")
 	void searchByName() {
-		step("Type search", () -> {
+		step("Тапнуть по полю поиска и ввести 'BrowserStack'", () -> {
 			$(AccessibilityId("Search Wikipedia")).click();
 			$(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).setValue("BrowserStack");
 		});
-		step("Verify content found", () ->
+		step("Проверить результаты поиска, что он не равен 0", () ->
 				$$(MobileBy.id("org.wikipedia.alpha:id/page_list_item_container"))
 						.shouldHave(sizeGreaterThan(0)));
 	}
